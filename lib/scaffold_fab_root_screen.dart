15d9929c3d2182b444e8eae59d24efb3091dba8e
Yuichi Fujiki <fjk89025@gmail.com>
2023-03-05 22:49:21 +0900

    Implemented Beamer push

--- lib/scaffold_fab_root_screen.dart
+++ lib/scaffold_fab_root_screen.dart
@@ -1,7 +1,15 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 
 class ScaffoldFabRootScreen extends StatelessWidget {
   const ScaffoldFabRootScreen({super.key});
+
+  static const List<Map<String, String>> data = [
+    {
+      "title": "Standard",
+      "path": "/scaffold/1",
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +18,28 @@ class ScaffoldFabRootScreen extends StatelessWidget {
         title: const Text("Scaffold"),
         backgroundColor: Colors.pink,
       ),
-      body: Container(
-        color: Colors.red,
+      body: ListView.separated(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          final e = data[index];
+
+          return ListTile(
+            title: Text(e["title"] ?? "-"),
+            onTap: () {
+              final path = e["path"];
+              if (path == null) return;
+              Beamer.of(context).beamToNamed(path);
+            },
+          );
+        },
+        separatorBuilder: (context, index) {
+          return SizedBox(
+            height: 1,
+            child: Container(
+              color: Colors.black12,
+            ),
+          );
+        },
       ),
     );
   }
