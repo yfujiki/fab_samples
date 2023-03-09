9c6bff8cfd378d0628aa1ae9d88289b4150d33c2
Yuichi Fujiki <fjk89025@gmail.com>
2023-03-09 17:40:24 +0900

    Added another alignment

--- lib/button_alignment.dart
+++ lib/button_alignment.dart
@@ -5,6 +5,7 @@ enum ButtonAlignment {
   centerLeft(Alignment.centerLeft),
   bottomLeft(Alignment.bottomLeft),
   topCenter(Alignment.topCenter),
+  center(Alignment.center),
   bottomCenter(Alignment.bottomCenter),
   topRight(Alignment.topRight),
   centerRight(Alignment.centerRight),
